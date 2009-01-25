797b5a6ebd85f14822b99b1b13584c366f4df8d8
holorose <holorose@4f9712e9-d43f-0410-8d4e-7b301817401d>
2009-01-25 11:06:24 +0000

    stack based allocator

---
+++ stack_allocator.h
@@ -0,0 +1,46 @@
+#ifndef RDESTL_STACK_ALLOCATOR_H
+#define RDESTL_STACK_ALLOCATOR_H
+
+#include "rdestl/rdestl.h"
+
+namespace rde
+{
+
+template<int TBytes>
+class stack_allocator
+{
+public:
+	explicit stack_allocator(const char* name = "STACK")
+	:	m_name(name), 
+		m_bufferTop(0)
+	{
+		/**/
+	}
+
+	void* allocate(size_t bytes, int /*flags*/ = 0)
+	{
+		RDE_ASSERT(m_bufferTop + bytes <= TBytes);
+		char* ret = &m_buffer[0] + m_bufferTop;
+		m_bufferTop += bytes;
+		return ret;
+	}
+	void deallocate(void* ptr, size_t /*bytes*/)
+	{
+		RDE_ASSERT(ptr == 0 || (ptr >= &m_buffer[0] && ptr < &m_buffer[TBytes]));
+		sizeof(ptr);
+	}
+
+	const char* get_name() const	{ return m_name; }
+
+private:
+	stack_allocator(const stack_allocator&);
+	stack_allocator& operator=(const stack_allocator&);
+
+	const char*	m_name;
+	char			m_buffer[TBytes];
+	size_t			m_bufferTop;
+};
+
+} // namespace rde
+//-----------------------------------------------------------------------------
+#endif 
