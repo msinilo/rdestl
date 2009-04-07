7c911c6f9173a2816c93a7e432a4261690876e9e
holorose <holorose@4f9712e9-d43f-0410-8d4e-7b301817401d>
2009-04-07 21:33:58 +0000

    Test cases for copying substrings

--- Test/FixedSubstringTest.cpp
+++ Test/FixedSubstringTest.cpp
@@ -18,6 +18,12 @@ namespace
 		rde::fixed_substring<char, 16> str("Hello world");
 		CHECK_EQUAL(0, rde::strcompare(str.data(), "Hello world"));
 	}
+	TEST(InequalityOp)
+	{
+		rde::fixed_substring<char, 9> str("Hello");
+		rde::fixed_substring<char, 9> str2("World");
+		CHECK(str2 != str);
+	}
 	TEST(CopyCtorGivesEqualStrings)
 	{
 		rde::fixed_substring<char, 16> str("Hello world");
@@ -30,6 +36,36 @@ namespace
 		rde::fixed_substring<char, 9> str("Hello ");
 		str.append("world");
 		CHECK_EQUAL(0, rde::strcompare(str.data(), "Hello wor"));
+	}
+	TEST(CopyCtorEqualSizes)
+	{
+		rde::fixed_substring<char, 9> str("Hello");
+		rde::fixed_substring<char, 9> str2(str);
+		CHECK(str == str2);
+	}
+	TEST(CopyCtorDifferentSizes)
+	{
+		rde::fixed_substring<char, 9> str("Hello");
+		rde::fixed_substring<char, 8> str2(str);
+		CHECK(str == str2);
+		rde::fixed_substring<char, 3> str3(str);
+		CHECK_EQUAL(0, rde::strcompare(str3.data(), "Hel"));
+	}
+	TEST(AssignmentOpEqualSizes)
+	{
+		rde::fixed_substring<char, 9> str("Hello");
+		rde::fixed_substring<char, 9> str2;
+		CHECK(str != str2);
+		str2 = str;
+		CHECK(str == str2);
+	}
+	TEST(AssignmentOpDifferentSizes)
+	{
+		rde::fixed_substring<char, 9> str("Hello");
+		rde::fixed_substring<char, 5> str2;
+		CHECK(str != str2);
+		str2 = str;
+		CHECK(str == str2);
 	}
 }
 
