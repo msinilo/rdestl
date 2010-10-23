7a57384a191285e80aea0c6fb2dd52a596e8af88
holorose <holorose@4f9712e9-d43f-0410-8d4e-7b301817401d>
2010-10-23 03:59:25 +0000

    Fixed COW string test

--- Test/StringTest.cpp
+++ Test/StringTest.cpp
@@ -5,14 +5,15 @@
 
 namespace
 {
-	typedef rde::basic_string<char, rde::allocator, rde::simple_string_storage<char, rde::allocator> > simple_string;
+	typedef rde::basic_string<char, rde::allocator, rde::simple_string_storage<char, rde::allocator> >	simple_string;
+	typedef rde::basic_string<char, rde::allocator, rde::cow_string_storage<char, rde::allocator> >		cow_string;
 
 #define CONCAT_(x, y)	x ## y
 #define CONCAT2_(x, y)	CONCAT_(x, y)
 #define TESTC(x)		TEST(CONCAT2_(x, POSTFIX))
 
 #define POSTFIX			COW
-#define STRING_CLASS	rde::string
+#define STRING_CLASS	cow_string
 #include "StringTestInc.h"
 
 #undef STRING_CLASS
