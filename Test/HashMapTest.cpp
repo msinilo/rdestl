b60237911deb8dba8df34d903ef99c519cafa2bc
holorose@gmail.com <holorose@gmail.com@4f9712e9-d43f-0410-8d4e-7b301817401d>
2012-09-02 04:21:49 +0000

    Fixed issue reported by Shiran Ben-Israel. Erasing entry from hash_map would sometimes fail

--- Test/HashMapTest.cpp
+++ Test/HashMapTest.cpp
@@ -6,7 +6,7 @@
 
 namespace
 {
-	struct hasher
+	struct hasher	
 	{
 		rde::hash_value_t operator()(const std::string& s) const
 		{
@@ -116,6 +116,54 @@ template rde::hash_map<std::string, int, hasher, 6>;
 		CHECK_EQUAL(8, m.size());
 		m.erase(m.begin(), m.end());
 		CHECK(m.empty());
+	}
+
+	// Reported by Shiran Ben-Israel
+	TEST(ShiranIssue)
+	{
+		static const int MAX_KEYS = 1024;
+		int aKeys[MAX_KEYS];
+		for (int i = 0; i < MAX_KEYS; i++)
+		{
+			int iValue = 0;
+			bool bInsertedIntoArray = false;
+			while (bInsertedIntoArray == false)
+			{
+				iValue = rand()%32767;
+				bool bAlreadyInArray = false;
+				for (int j = 0; j < i; j++)
+				{
+					if (aKeys[j] == iValue)
+					{
+						bAlreadyInArray = true;
+					}
+					if (bAlreadyInArray == true)
+						break;
+				}
+				if (bAlreadyInArray==false)
+				{
+					aKeys[i] = iValue;
+					bInsertedIntoArray = true;
+				}
+			}
+		}
+		typedef rde::hash_map<int, int> HashMap;
+		typedef rde::hash_map<int, int>::iterator HashMapIterator;
+		HashMap m_mHashMap;
+		HashMapIterator itr = m_mHashMap.begin();
+
+		for (int i = 0; i < MAX_KEYS; i++)
+		{
+			m_mHashMap.insert(rde::make_pair(aKeys[i],0));
+		}
+ 
+		for (int i = 0; i < MAX_KEYS; i++)
+		{
+			m_mHashMap.erase(aKeys[i]);
+			HashMapIterator itr = m_mHashMap.find(aKeys[i]);
+			RDE_ASSERT(itr == m_mHashMap.end());
+			CHECK(itr == m_mHashMap.end());
+		}
 	}
 }	
 
