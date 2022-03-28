dbe275f05a7a11769d83ab34473b8076395f2ccd
Stelio Kontos <37424493+StelioKontosXBL@users.noreply.github.com>
2022-03-27 23:48:28 -0500

    add fill test, split ctor tests

--- Test/FixedArrayTest.cpp
+++ Test/FixedArrayTest.cpp
@@ -6,20 +6,25 @@ namespace
 {
 TEST_CASE("fixed_array", "[array]")
 {
-	SECTION("Size")
-	{
-		rde::fixed_array<int, 5> a;
-		CHECK(5 == a.size());
-	}
-
-	// https://github.com/msinilo/rdestl/issues/17
-	SECTION("Aggregate initialization")
+	SECTION("Construction")
 	{
 		const int a[] ={ 1, 2, 3, 4, 5 };
-		rde::fixed_array<int, 5> arr;
-		arr.from_raw_array(a);
-		CHECK(5 == arr.size());
-		CHECK(0 == memcmp(arr.data(), &a[0], sizeof(a)));
+
+		// https://github.com/msinilo/rdestl/issues/17
+		SECTION("Aggregate initialization")
+		{
+			rde::fixed_array<int, 5> arr ={ 1, 2, 3, 4, 5 };
+			CHECK(5 == arr.size());
+			CHECK(0 == memcmp(arr.data(), &a[0], sizeof(a)));
+		}
+
+		SECTION("ArrayCtor")
+		{
+			rde::fixed_array<int, 5> arr;
+			arr.from_raw_array(a);
+			CHECK(5 == arr.size());
+			CHECK(0 == memcmp(arr.data(), &a[0], sizeof(a)));
+		}
 	}
 
 	SECTION("Accessors")
@@ -33,7 +38,7 @@ TEST_CASE("fixed_array", "[array]")
 			CHECK(6 == arr[2]);
 		}
 
-		SECTION("Front/Back)")
+		SECTION("Front/Back")
 		{
 			CHECK(1 == arr.front());
 			CHECK(5 == arr.back());
@@ -44,17 +49,19 @@ TEST_CASE("fixed_array", "[array]")
 		}
 	}
 
-	SECTION("Modifiers")
+	SECTION("Size")
 	{
-		rde::fixed_array<int, 5> arr ={ 0, 0, 0, 0, 0 };
-		const int a[] ={ 1, 1, 1, 1, 1 };
+		rde::fixed_array<int, 5> a;
+		CHECK(5 == a.size());
+	}
 
-		SECTION("fill")
-		{
-			REQUIRE(0 != memcmp(arr.data(), &a[0], sizeof(a)));
-			arr.fill(1);
-			CHECK(0 == memcmp(arr.data(), &a[0], sizeof(a)));
-		}
+	SECTION("Fill")
+	{
+		const int a[] = { 1, 1, 1, 1, 1 };
+		rde::fixed_array<int, 5> arr = { 0, 0, 0, 0, 0 };
+		REQUIRE(0 != memcmp(arr.data(), &a[0], sizeof(a)));
+		arr.fill(1);
+		CHECK(0 == memcmp(arr.data(), &a[0], sizeof(a)));
 	}
 }
 }
